745b23251a5a353b8d9d398f79902a43fe7cf48f
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2019-11-05 11:57:50 -0500

    Add files via upload

---
+++ runstateless.sh
@@ -0,0 +1,1 @@
+/Users/tspann/Documents/nifi-1.10.0-SNAPSHOT/bin/nifi.sh stateless RunFromRegistry Once --file /Users/tspann/Documents/nifi-1.10.0-SNAPSHOT/logs/kafka.json
---
+++ runstatelessc.sh
@@ -0,0 +1,1 @@
+/Users/tspann/Documents/nifi-1.10.0-SNAPSHOT/bin/nifi.sh stateless RunFromRegistry Continuous --file /Users/tspann/Documents/nifi-1.10.0-SNAPSHOT/logs/kafkaconsumer.json
